caf57c90ed096a26d68fc1c9ba376572150833a7
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-07-22 20:35:40 -0300

    Configuracao de banco de dados inicial

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -20,12 +20,12 @@ dotenv.config();
 //})
 
 export const AppDataSource = new DataSource({
-    type: process.env.DATABASE_TYPE as any,
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     synchronize: true,
     logging: false,
     entities: [Post],
